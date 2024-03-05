b3a8d93c249d17abc39772b2e3de5169ae06e97f
Alex Popescu <alexp@gmail.com>
2024-03-05 15:20:04 +0200

    reparat bucla infinita

--- lab2.c
+++ lab2.c
@@ -1,20 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int function(int x){
-	x=x-2;
-	x=function(x);
-	return x;
+int function(int x)
+{
+    x=x-2;
+    if(x<0)
+        x=function(x);
+    return x;
 }
 
-int main(){
-	int a=30, i=0;
+int main()
+{
+    int a=30, i=0;
 
-	while (a>0){
-		a=function(a);
-		i++;
-	}
-	printf("%d", i);
-	return 0;
+    while (a>0)
+    {
+        a=function(a);
+        i++;
+    }
+    printf("%d", i);
+    return 0;
 }
 
